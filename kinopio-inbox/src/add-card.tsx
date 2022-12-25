da0ae21d17392162987a51f0696f2f868f4652c6
pirijan <hi@pirijan.com>
2022-12-25 10:49:13 -0500

    saves card to inbox via api

--- kinopio-inbox/src/add-card.tsx
+++ kinopio-inbox/src/add-card.tsx
@@ -1,7 +1,9 @@
 import { Form, ActionPanel, Action, Toast, showToast, getPreferenceValues } from "@raycast/api";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import got from "got";
 
 const cardLimit = 300
+const host = 'https://api.kinopio.club'
 
 type Values = {
   name: string;
@@ -10,6 +12,7 @@ type Values = {
 export default function Command(props: LaunchProps<{ draftValues: Values }>) {
   const { draftValues } = props;
   const [nameError, setNameError] = useState<string | undefined>();
+  const textFieldRef = useRef<Form.TextField>(null);
 
   const preferences = getPreferenceValues<Preferences>();
   console.log('🙈', preferences.apiKey);
@@ -30,16 +33,33 @@ export default function Command(props: LaunchProps<{ draftValues: Values }>) {
       title: "Saving card",
     });
     try {
-      // TODO await: fetch save the card w preferences.apiKey 
+      const url = `${host}/card/to-inbox`
+      await got.post({
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Cache-Control': 'must-revalidate, no-store, no-cache, private',
+          'Authorization': preferences.apiKey          
+        },
+        json: { name: values.name },
+        responseType: "json"
+      });
+      console.log('🐸')
+      textFieldRef.current?.reset()
       toast.style = Toast.Style.Success;
       toast.title = "Saved card to your inbox";
     } catch (error) {
+      console.error('🚒', error)
       toast.style = Toast.Style.Failure;
       toast.title = "Failed to save card";
       if (error instanceof Error) {
         toast.message = error.message;
       }
     }
+  }
+
+  function resetTextarea() {
+
   }
 
   return (
@@ -61,6 +81,7 @@ export default function Command(props: LaunchProps<{ draftValues: Values }>) {
         onChange={validateName}
         error={nameError}
         info="Markdown is supported"
+        ref={textFieldRef}
       />
     </Form>
   );
